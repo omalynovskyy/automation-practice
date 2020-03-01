7a4fe112faf0c98c1ecf0fb520208936903696cd
omalynovskyy <oleksiy.malynovskyy@gmail.com>
2020-03-02 00:57:17 +0200

    lesson09 CustomExpectecComditions

---
+++ src/test/java/lesson09/custom_expected_comdition/Test_Conditions.java
@@ -0,0 +1,59 @@
+package lesson09.custom_expected_comdition;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class Test_Conditions {
+
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp() {
+        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\omalynovskyy\\Documents\\Soft\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+
+        driver.get("http://automationpractice.com/index.php");
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        driver.quit();
+    }
+
+
+    @Test
+    @Ignore
+    public void verifyFirstTipIsCorrect_viaAssertTrue() {
+        driver.findElement(By.id("search_query_top"))
+                .clear();
+        driver.findElement(By.id("search_query_top"))
+                .sendKeys("Dress");
+
+        WebElement fifthTip = (new WebDriverWait(driver, 10))
+                .until(CustomExpectecComdition.listNthElementHasTest(By.xpath("//*[@id=\"index\"]/div[2]/ul/li"),
+                        4,
+                        "Dress"
+                ));
+
+    }
+
+    @Test
+    public void verifyPageLoad(){
+        String pageLoad = (new WebDriverWait(driver, 10))
+                .until(CustomExpectecComdition.pageIsLoaded("autom","Store"));
+    }
+
+}
