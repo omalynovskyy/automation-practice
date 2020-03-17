ec76f3e36fbec226d37ada2f129d9fafb990a366
omalynovskyy <oleksiy.malynovskyy@gmail.com>
2020-03-17 17:30:39 +0200

    lesson10 page factory

--- src/test/java/lesson10_pFactory/TestLoginAndAccount.java
+++ src/test/java/lesson10_pFactory/TestLoginAndAccount.java
@@ -11,8 +11,8 @@ import java.util.concurrent.TimeUnit;
 public class TestLoginAndAccount {
     static WebDriver driver;
 
-    LoginPage chainedLoginPage = new LoginPage(driver);
-    AccountPage chainedAccountPage = new AccountPage(driver);
+    LoginPage chainedLoginPage;
+    AccountPage chainedAccountPage;
 
 
     @BeforeClass
@@ -28,8 +28,25 @@ public class TestLoginAndAccount {
         driver.quit();
     }
 
+//    @Test
+    //этот тест содержит все три теста в одном методе - он проходит успешно
+//    public void allInOne(){
+//        LoginPage loginPage = new LoginPage(driver);
+//        loginPage.openLoginPage();
+//        Assert.assertThat(driver.getTitle(), CoreMatchers.containsString("Login"));
+//        chainedAccountPage = loginPage.login("email4test@gmail.com", "passwd4test");
+//        Assert.assertThat(driver.getTitle(), CoreMatchers.containsString("My account"));
+//        chainedLoginPage = chainedAccountPage.signOut();
+//        Assert.assertThat(driver.getTitle(), CoreMatchers.containsString("Login"));
+//        chainedLoginPage.enterUserName("email4test@gmail.com");
+//        chainedLoginPage.enterPassword("passwd4test");
+//        chainedLoginPage.clickSignInBtn();
+//        Assert.assertThat(driver.getTitle(), CoreMatchers.containsString("My account"));
+
+//    }
+
     @Test
-    //@Ignore
+    //Этот тест выполняется успешно
     public void loginByOneMthd(){
         LoginPage loginPage = new LoginPage(driver);
         loginPage.openLoginPage();
@@ -40,7 +57,36 @@ public class TestLoginAndAccount {
     }
 
     @Test
-    //@Ignore
+    //Этот тест валится (даже если начать его с того чем успешно заканчивается предідущие) и как следствие следующий тест тоже валится
+    /*
+    java.lang.NullPointerException
+	at lesson10_pFactory.TestLoginAndAccount.signOutTest(TestLoginAndAccount.java:63)
+	at java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
+	at java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)
+	at java.base/jdk.internal.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
+	at java.base/java.lang.reflect.Method.invoke(Method.java:567)
+	at org.junit.runners.model.FrameworkMethod$1.runReflectiveCall(FrameworkMethod.java:50)
+	at org.junit.internal.runners.model.ReflectiveCallable.run(ReflectiveCallable.java:12)
+	at org.junit.runners.model.FrameworkMethod.invokeExplosively(FrameworkMethod.java:47)
+	at org.junit.internal.runners.statements.InvokeMethod.evaluate(InvokeMethod.java:17)
+	at org.junit.runners.ParentRunner.runLeaf(ParentRunner.java:325)
+	at org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:78)
+	at org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:57)
+	at org.junit.runners.ParentRunner$3.run(ParentRunner.java:290)
+	at org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:71)
+	at org.junit.runners.ParentRunner.runChildren(ParentRunner.java:288)
+	at org.junit.runners.ParentRunner.access$000(ParentRunner.java:58)
+	at org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:268)
+	at org.junit.internal.runners.statements.RunBefores.evaluate(RunBefores.java:26)
+	at org.junit.internal.runners.statements.RunAfters.evaluate(RunAfters.java:27)
+	at org.junit.runners.ParentRunner.run(ParentRunner.java:363)
+	at org.junit.runner.JUnitCore.run(JUnitCore.java:137)
+	at com.intellij.junit4.JUnit4IdeaTestRunner.startRunnerWithArgs(JUnit4IdeaTestRunner.java:68)
+	at com.intellij.rt.junit.IdeaTestRunner$Repeater.startRunnerWithArgs(IdeaTestRunner.java:33)
+	at com.intellij.rt.junit.JUnitStarter.prepareStreamsAndStart(JUnitStarter.java:230)
+	at com.intellij.rt.junit.JUnitStarter.main(JUnitStarter.java:58)
+
+     */
     public void signOutTest(){
         //Assert.assertThat(driver.getTitle(), CoreMatchers.containsString("My account"));
         chainedLoginPage = chainedAccountPage.signOut();
@@ -48,21 +94,14 @@ public class TestLoginAndAccount {
 
     }
 
-
     @Test
-    //@Ignore
+    //
     public void loginByThreeMthd(){
-        //LoginPage loginPage = new LoginPage(driver);
-        //loginPage.openLoginPage(); - почему с этим не работает, что не так?
         Assert.assertThat(driver.getTitle(), CoreMatchers.containsString("Login"));
         chainedLoginPage.enterUserName("email4test@gmail.com");
         chainedLoginPage.enterPassword("passwd4test");
         chainedLoginPage.clickSignInBtn();
         Assert.assertThat(driver.getTitle(), CoreMatchers.containsString("My account"));
-
     }
-
-
-
 
 }
